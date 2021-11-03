40841875ceb4e78fa9f858bffd397491ecda4fe6
Roma <romaqwer08@gmail.com>
2021-11-03 10:32:02 +0200

    add proxy to fix cors

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,5 @@
-import fetchDefaults from 'fetch-defaults';
-
-const fetchApi = fetchDefaults(
-  fetch,
-  'https://cors-anywhere.herokuapp.com/https://gameinfo.albiononline.com/api/',
-  {
-    headers: {
-      // mode: 'no-cors',
-      // referrerPolicy: 'no-referrer',
-      // 'Access-Control-Allow-Origin': 'http://localhost:3000',
-    },
-  }
-);
-
 export const getPlayers = (offset) => {
-  return fetchApi(
-    `gameinfo/events/playerweaponfame?limit=10&offset=${offset}&range=lastMonth&weaponCategory=bow`
+  return fetch(
+    `api/gameinfo/events/playerweaponfame?limit=10&offset=${offset}&range=lastMonth&weaponCategory=bow`
   ).then((res) => res.json());
 };
